5f5689c093c5308c4c903e279d360ee3fe433215
Bejawada Srinadh <2100030051cseh@gmail.com>
2026-04-28 22:47:09 +0530

    MenuItem,Resta,Cart model classes

---
+++ FoodDeliveryApplicationTomato/src/models/MenuItem.java
@@ -0,0 +1,38 @@
+package models;
+
+public class MenuItem {
+    
+    private String code;
+    private String name;
+    private int price;
+
+    public MenuItem(String code, String name, int price) {
+        this.code = code;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
---
+++ FoodDeliveryApplicationTomato/src/models/Restaurant.java
@@ -0,0 +1,43 @@
+package models;
+
+import java.util.*;
+
+public class Restaurant {
+    
+    private static int nextRestaurantId = 0;
+    
+    private int restaurantId = 0;
+    private String name;
+    private String location;
+    private List<MenuItem> menuItemList;
+
+    public Restaurant(String location, String name) {
+        this.location = location;
+        this.name = name;
+        this.restaurantId = ++nextRestaurantId;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    
+    public void addMenuItem(MenuItem item){
+        menuItemList.add(item);
+    }
+    
+    public List<MenuItem> getMenu(){
+        return menuItemList;
+    }
+}
